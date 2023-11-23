7647370e45bab0560775e932d7ee6b36c721a038
Karantooo <ctomasalvarezn@gmail.com>
2023-11-23 20:28:43 -0300

    feat: Implementacion pasajero y enum estadoAsientos

--- src/main/java/org/example/Logica/EstadoAsiento.java
+++ src/main/java/org/example/Logica/EstadoAsiento.java
@@ -1,4 +1,5 @@
 package org.example.Logica;
 
 public enum EstadoAsiento {
+    OCUPADO, DISPONIBLE;
 }
--- src/main/java/org/example/Logica/Pasajero.java
+++ src/main/java/org/example/Logica/Pasajero.java
@@ -1,4 +1,31 @@
 package org.example.Logica;
 
 public class Pasajero {
+    private String nombre;
+    private String apellido;
+    private String banco;
+    private String numCuenta;
+
+    public Pasajero(String nombre, String apellido, String banco, String numCuenta) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.banco = banco;
+        this.numCuenta = numCuenta;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getBanco() {
+        return banco;
+    }
+
+    public String getNumCuenta() {
+        return numCuenta;
+    }
 }
