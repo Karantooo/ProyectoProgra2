674edb812ee070fefffd1b98148cf0d903ab114d
Karantooo To <ctomasalvarezn@gmail.com>
2023-11-30 11:32:15 -0300

    docs: Se añade la documentacion de las clases bus y director bus

--- src/main/java/org/example/Logica/DirectorBus.java
+++ src/main/java/org/example/Logica/DirectorBus.java
@@ -1,18 +1,39 @@
 package org.example.Logica;
 
+/**
+ * Clase director para instanciar ciertos buses por defecto.
+ * La idea es para ghenerar buses predeterminados para los casos iniciales por defecto.
+ * @author Benjamin Espinoza
+ */
 public class DirectorBus {
+
+    /**
+     * Bus por defecto de tamaño pequeño.
+     * @param recorrido
+     * @return
+     */
     public Bus buildChico(Recorrido recorrido){
         Bus.BusBuilder busBuilder = new Bus.BusBuilder(recorrido);
         busBuilder.buildPisos(1);
         busBuilder.buildAsientosPorPiso(40);
         return busBuilder.buildBus();
     }
+    /**
+     * Bus por defecto de tamaño mediano.
+     * @param recorrido
+     * @return
+     */
     public Bus buildMediano(Recorrido recorrido){
         Bus.BusBuilder busBuilder = new Bus.BusBuilder(recorrido);
         busBuilder.buildPisos(1);
         busBuilder.buildAsientosPorPiso(60);
         return busBuilder.buildBus();
     }
+    /**
+     * Bus por defecto de tamaño grande.
+     * @param recorrido
+     * @return
+     */
     public Bus buildGrande(Recorrido recorrido){
         Bus.BusBuilder busBuilder = new Bus.BusBuilder(recorrido);
         busBuilder.buildPisos(2);
