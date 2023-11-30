48b54087f7f3980d6ec5135f577e5ca9d984c657
Karantooo To <ctomasalvarezn@gmail.com>
2023-11-30 11:07:18 -0300

    docs: Se añade Documentacion director asiento

--- src/main/java/org/example/Logica/DirectorAsiento.java
+++ src/main/java/org/example/Logica/DirectorAsiento.java
@@ -1,11 +1,22 @@
 package org.example.Logica;
 
+/**
+ * Clase ideada para hacer ciertos builds por defecto que se van a utilizar para que generar las diferentes
+ * categorias de los asientos.
+ * @author Carlos Tomas Alvarez Norambuena
+ */
+
 public class DirectorAsiento {
 
     public DirectorAsiento(){
 
     }
 
+    /**
+     * Build por defecto para crear asientos premium
+     * @param codigoAsiento codigo asociado al asiento
+     * @return asiento premium
+     */
     public Asiento buildPremium(String codigoAsiento){
         Asiento.Builder builder = new Asiento.Builder(EstadoAsiento.DISPONIBLE, codigoAsiento, 1500);
         builder.setSistemaEntretenimiento(false);
@@ -13,7 +24,11 @@ public class DirectorAsiento {
         builder.setDescripcionAsiento("Asiento Premium");
         return builder.buildAsiento();
     }
-
+    /**
+     * Build por defecto para crear asientos economico
+     * @param codigoAsiento codigo asociado al asiento
+     * @return asiento economico
+     */
     public Asiento buildEconomico(String codigoAsiento){
         Asiento.Builder builder = new Asiento.Builder(EstadoAsiento.DISPONIBLE, codigoAsiento, 1000);
         builder.setSistemaEntretenimiento(false);
@@ -21,7 +36,11 @@ public class DirectorAsiento {
         builder.setDescripcionAsiento("Asiento Economico");
         return builder.buildAsiento();
     }
-
+    /**
+     * Build por defecto para crear asientos ejecutivo
+     * @param codigoAsiento codigo asociado al asiento
+     * @return asiento ejecutivo
+     */
     public Asiento buildEjecutivo(String codigoAsiento){
         Asiento.Builder builder = new Asiento.Builder(EstadoAsiento.DISPONIBLE, codigoAsiento, 2000);
         builder.setSistemaEntretenimiento(true);
