0c25e70baaebe0079a9abfa5c02a95b7cda13d6f
Benjamin Espinoza <beespinoza2022@inf.udec.cl>
2023-11-30 00:46:07 -0300

    feat: estructura inicial para los paneles de la seleccion de asientos

---
+++ src/main/java/org/example/Vistas/PanelAsiento.java
@@ -0,0 +1,12 @@
+package org.example.Vistas;
+
+import org.example.Logica.Asiento;
+
+import javax.swing.*;
+
+public class PanelAsiento extends JPanel {
+    private Asiento asiento;
+    public PanelAsiento(Asiento asiento){
+        this.asiento = asiento;
+    }
+}
---
+++ src/main/java/org/example/Vistas/PanelSeleccionAsiento.java
@@ -0,0 +1,12 @@
+package org.example.Vistas;
+
+import org.example.Logica.Bus;
+
+import javax.swing.*;
+
+public class PanelSeleccionAsiento extends JPanel {
+    private Bus bus;
+    public PanelSeleccionAsiento(Bus bus){
+        this.bus = bus;
+    }
+}
