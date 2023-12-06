f891e24a4f35a9d1995af8bdd0f97a5b373cf484
Karantooo To <ctomasalvarezn@gmail.com>
2023-12-06 09:36:46 -0300

    docs: Se añade documentacion de diferentes clases de vistas

--- src/main/java/org/example/Vistas/PanelCorridaAsientos.java
+++ src/main/java/org/example/Vistas/PanelCorridaAsientos.java
@@ -6,9 +6,20 @@ import org.example.Logica.CorridaAsientos;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * Clase para instanciar el panel en el que se muestran las corridas de asientos.
+ * @author Benjamin Espinoza
+ */
 public class PanelCorridaAsientos extends JPanel {
     private CorridaAsientos corridaAsientos;
     private PanelReservarAsiento panelReservarAsiento;
+
+    /**
+     * Se establece un GridLayout en el que se puede visualizar en cada casilla el asiento correspondiente.
+     * Se utiliza el objeto corridaAsientos para establecer la informacion especifica de cada asiento.
+     * @param corridaAsientos
+     * @param panelReservarAsiento
+     */
     public PanelCorridaAsientos(CorridaAsientos corridaAsientos, PanelReservarAsiento panelReservarAsiento){
         this.corridaAsientos = corridaAsientos;
         this.panelReservarAsiento = panelReservarAsiento;
